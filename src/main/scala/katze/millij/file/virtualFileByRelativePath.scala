48a0c6cc4fa79d9747c3f59d028b596b4d40853a
Katze <googletan@mail.ru>
2026-07-16 19:35:20 +0300

    Add virtualFileByRelativePath

---
+++ src/main/scala/katze/millij/file/virtualFileByRelativePath.scala
@@ -0,0 +1,21 @@
+package katze.millij.file
+
+import cats.{Foldable, Show}
+import com.intellij.openapi.project.{Project, ProjectUtil}
+import com.intellij.openapi.vfs.VirtualFile
+import cats.syntax.all.*
+import katze.millij.data.SegmentedPath
+
+def virtualFileByRelativePath(project: Project, relativePath: String) : Option[VirtualFile] =
+  for
+    baseDir <- Option(ProjectUtil.guessProjectDir(project))
+    file <- Option(baseDir.findFileByRelativePath(relativePath))
+  yield file
+end virtualFileByRelativePath
+
+def virtualFileByRelativePath[F[_] : Foldable, T : Show](
+  project: Project,
+  relativePath: SegmentedPath[F, T]
+) : Option[VirtualFile] =
+  virtualFileByRelativePath(project, relativePath.asFilePath)
+end virtualFileByRelativePath
