8cc1db637d8bdcf0b5431e0e72f529ddebf4d461
Katze <googletan@mail.ru>
2026-07-17 11:26:50 +0300

    Optimise imports in MillProjectStartupActivity

--- src/main/scala/katze/millij/MillProjectStartupActivity.scala
+++ src/main/scala/katze/millij/MillProjectStartupActivity.scala
@@ -1,14 +1,9 @@
 package katze.millij
 
-import _root_.kotlin.coroutines.Continuation
-import com.intellij.jarRepository.{RemoteRepositoriesConfiguration, RemoteRepositoryDescription}
 import com.intellij.openapi.externalSystem.autoimport.ExternalSystemProjectTracker
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.{ProjectActivity, StartupActivity}
+import com.intellij.openapi.startup.StartupActivity
 import katze.millij.externalSystem.MillBspProjectAware
-
-import java.nio.file.{Files, Paths}
-import scala.jdk.CollectionConverters.*
 
 final class MillProjectStartupActivity extends StartupActivity.DumbAware:
   override def runActivity(project: Project): Unit =
